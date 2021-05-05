219fbbdef9032dd0acba3ddd4e3c7f7947f79d3e
Cory Forsyth <cory.forsyth@addepar.com>
2021-05-05 14:33:30 -0400

    use GITHUB_RECORDING_TOKEN when recording requests

--- jest.setup.js
+++ jest.setup.js
@@ -1,14 +1,14 @@
 global.td = require('testdouble');
 require('testdouble-jest')(td, jest);
 
+process.env.GITHUB_OWNER = 'bantic';
+process.env.GITHUB_REPO = 'github-metrics-tests';
+
 if (process.env.RECORD_REQUESTS) {
-  for (let requiredEnvVar of ['GITHUB_OWNER', 'GITHUB_REPO', 'GITHUB_TOKEN']) {
-    if (!process.env[requiredEnvVar]) {
-      throw new Error(`Missing required process.env.${requiredEnvVar}`);
-    }
+  if (!process.env.GITHUB_RECORDING_TOKEN) {
+    throw new Error('Missing required process.env.GITHUB_RECORDING_TOKEN');
   }
+  process.env.GITHUB_TOKEN = process.env.GITHUB_RECORDING_TOKEN;
 } else {
-  process.env.GITHUB_OWNER = 'bantic';
-  process.env.GITHUB_REPO = 'github-metrics-tests';
   process.env.GITHUB_TOKEN = '*****';
 }
