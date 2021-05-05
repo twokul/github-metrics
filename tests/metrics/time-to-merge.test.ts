697ab6ef3c3cbf41933c441e22f4f29ff569054a
Cory Forsyth <cory.forsyth@addepar.com>
2021-05-05 13:05:19 -0400

    add test for no-data for TimetomergeMetric

--- tests/metrics/time-to-merge.test.ts
+++ tests/metrics/time-to-merge.test.ts
@@ -23,4 +23,15 @@ describe('metric: TimeToMerge', () => {
     expect(p90).toBe(168000);
     expect(p100).toBe(168000);
   });
+
+  test('no problems for test repo when no merged PRs are found', async () => {
+    let start = DateTime.fromISO('2021-05-04T18:00:00Z');
+    let end = start.plus({ minutes: 1 });
+    let interval = Interval.fromDateTimes(start, end);
+
+    let metric = new TimeToMergeMetric(interval);
+    await metric.run();
+
+    expect(metric.data.length).toBe(0);
+  });
 });
