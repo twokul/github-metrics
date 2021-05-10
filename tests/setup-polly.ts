5fbbee450f9f2ad020b8b40b7e01d40cae88bc37
Cory Forsyth <cory.forsyth@addepar.com>
2021-05-10 15:48:36 -0400

    normalize requests

--- tests/setup-polly.ts
+++ tests/setup-polly.ts
@@ -1,18 +1,70 @@
 import * as path from 'path';
 
-import replaceAll from 'string.prototype.replaceall';
-
 // @ts-ignore
 import { setupPolly } from 'setup-polly-jest';
 
 // @ts-ignore
 import FSPersister from '@pollyjs/persister-fs';
 
+type Header = {
+  name: string;
+  value: string;
+};
+
+type HeaderDefaults = {
+  [key: string]: string;
+};
+
+function normalizeHeaders(
+  headers: Header[],
+  defaults: HeaderDefaults
+): Header[] {
+  return headers.map((header: any) => {
+    if (header.name in defaults) {
+      header.value = defaults[header.name];
+    }
+    return header;
+  });
+}
+
+function normalizeRequestHeaders(headers: Header[]): Header[] {
+  let defaults: HeaderDefaults = {
+    authorization: 'token *****',
+  };
+  return normalizeHeaders(headers, defaults);
+}
+
+function normalizeResponseHeaders(headers: Header[]): Header[] {
+  const defaults: HeaderDefaults = {
+    'x-ratelimit-limit': '5000',
+    'x-ratelimit-used': '0',
+    'x-ratelimit-remaining': '5000',
+    'x-ratelimit-reset': '1620677342',
+    date: 'Mon, 10 May 2021 19:12:14 GMT',
+    'x-github-request-id': 'D403:6EA6:6FD290:1B2A629:60997471',
+  };
+  return normalizeHeaders(headers, defaults);
+}
+
 class TokenStrippingPersister extends FSPersister {
-  stringify(...args: any[]) {
-    let string = super.stringify(...args);
-    string = replaceAll(string, /"token .*?"/g, '"token *****"');
-    return string;
+  stringify(har: any) {
+    let entries = har.log.entries;
+    for (let entry of entries) {
+      entry.request.headers = normalizeRequestHeaders(entry.request.headers);
+      entry.response.headers = normalizeResponseHeaders(entry.response.headers);
+      entry.startedDateTime = '2021-05-10T17:59:14.813Z';
+      entry.time = 150;
+      entry.timings = {
+        blocked: -1,
+        connect: -1,
+        dns: -1,
+        receive: 0,
+        send: 0,
+        ssl: -1,
+        wait: 150,
+      };
+    }
+    return super.stringify(har);
   }
 }
 
