1bc00e049f555e1cec004754d02b5cf162f7df6a
Cory Forsyth <cory.forsyth@addepar.com>
2022-11-03 15:12:16 +0100

    avoid using "as string" in ts for `indent` function

--- src/utils/indent.ts
+++ src/utils/indent.ts
@@ -1,10 +1,12 @@
 export const TAB = '    ';
+export function indent(count: number, input: string): string;
+export function indent(count: number, input: string[]): string[];
 export function indent(
   count: number,
   input: string | string[]
 ): string | string[] {
   if (Array.isArray(input)) {
-    return input.map((str) => indent(count, str) as string);
+    return input.map((str) => indent(count, str));
   } else {
     return TAB.repeat(count) + input;
   }
